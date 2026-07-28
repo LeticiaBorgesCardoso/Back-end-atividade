30ae68eb36aa2ad6ffb49965c71291ac84970aca
Leticia Borges <leticiacardosoborges07@gmail.com>
2026-07-27 22:04:44 -0300

    Atividade 1 e 2

--- Atividade01.php
+++ Atividade01.php
@@ -1,1 +1,12 @@
 <?php
+
+// variaveis 
+
+$idade = 20; 
+
+
+if($idade >= 18) {
+    echo"Voce pode comprar cigarro e cachaca no mercado. \n";
+} else {
+    echo "Voce tem que pedir pro seu amiguinho comprar cigarro pra voce \n";
+}
---
+++ Atividade02.php
@@ -0,0 +1,15 @@
+<?php
+
+$dinheiro = 200000; 
+
+if($dinheiro <=1000) {
+    echo"seu miseravel";
+} else if ($dinheiro <= 5000) {
+    echo " Seu médio"; 
+} else if ($dinheiro <= 10000) {
+    echo "Riquin"; 
+} else if ($dinheiro <= 15000) {
+    echo "Ricon peteco peteco";
+} else ($dinheiro >= 200000); {
+echo "Milionario elon muski";
+} 
